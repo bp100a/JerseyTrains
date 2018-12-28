6b2e99824077ee73e97adca5dcbdc55362fa2104
bp100a <bp100a@hotmail.com>
2018-12-28 16:55:53 -0500

    more tests for redis

--- tests/models/test_redis.py
+++ tests/models/test_redis.py
@@ -24,10 +24,15 @@ class TestRedis(TestCase):
         assert redis_password == 'bogus.redis.password'
         assert redis_port == 14405
 
-    def test_redis_initialize(self):
+    def test_redis_initialize_fake(self):
         fake = fakeredis.FakeStrictRedis()
         cloudredis.initialize_cloud_redis(injected_server=fake)
         assert cloudredis.REDIS_SERVER == fake
+
+    def test_redis_initialize(self):
+        cloudredis.REDIS_SERVER = None
+        cloudredis.initialize_cloud_redis(injected_server=None)
+        assert cloudredis.REDIS_SERVER
 
     def test_redis_cache_station_list(self):
         fake = fakeredis.FakeStrictRedis()
