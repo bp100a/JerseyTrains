49432f255d3a5c58d6a069f245002dfaa2b5b8a1
bp100a <bp100a@hotmail.com>
2018-12-28 13:53:34 -0500

    remove beerlister test code

--- deploy/validate-build.sh
+++ deploy/validate-build.sh
@@ -6,21 +6,5 @@
 #   validate_build.sh
 #
 
-aws lambda invoke --invocation-type RequestResponse --function-name JerseyTrains --qualifier STAGE --region us-east-1 --payload file://tests/data/ListBreweries.json ListBreweries.out
-if ! grep -q 'Here are the breweries I know' ListBreweries.out; then
-   exit 1
-fi
-
-aws lambda invoke --invocation-type RequestResponse --function-name JerseyTrains --qualifier STAGE --region us-east-1 --payload file://tests/data/GetTapListIntent_TwinElephant.json TwinElephant.out
-if ! grep -q 'on tap at Twin Elephant' TwinElephant.out; then
-   exit 2
-fi
-
-aws lambda invoke --invocation-type RequestResponse --function-name JerseyTrains --qualifier STAGE --region us-east-1 --payload file://tests/data/GetTapListIntent_Alementary.json Alementary.out
-if ! grep -q 'on tap at Alementary' Alementary.out; then
-   cat Alementary.out
-   exit 3
-fi
-
 # nothing wrong, clean exit
 exit 0
