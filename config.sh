2b101bd33da5b1e78fd899d54e53cee368e7f724
bp100a <bp100a@hotmail.com>
2018-12-28 13:07:26 -0500

    create configuration directory

--- config.sh
+++ config.sh
@@ -6,6 +6,20 @@ set -o nounset
 # Exit script if a statement returns a non-true return value.
 set -o errexit
 
+mkdir configuration
+cd configuration
+
+# generate a configuration file from environment variables
+# (overwrite bogus file that already exists)
+echo \"\"\"secret values known only to DevOps\"\"\" > prod_config.py
+echo "BUILD_NUMBER =" $CIRCLE_BUILD_NUM >> prod_config.py
+echo "REDIS_HOST =" \"$REDIS_HOST\" >> prod_config.py
+echo "REDIS_PORT =" $REDIS_PORT >> prod_config.py
+echo "REDIS_PASSWORD =" \"$REDIS_PASSWORD\" >> prod_config.py
+echo "USERNAME = " \"$NJT_USERNAME\" >> prod_config.py
+echo "APIKEY = " \"$NJT_APIKEY\" >> prod_config.py
+echo "HOSTNAME =" \"$NJT_URL\" >> prod_config.py
+
 # generate a configuration file from environment variables
 # (overwrite bogus file that already exists)
 echo \"\"\"secret values known only to DevOps\"\"\" > config.py
@@ -13,3 +27,6 @@ echo "BUILD_NUMBER =" $CIRCLE_BUILD_NUM >> config.py
 echo "REDIS_HOST =" \"$REDIS_HOST\" >> config.py
 echo "REDIS_PORT =" $REDIS_PORT >> config.py
 echo "REDIS_PASSWORD =" \"$REDIS_PASSWORD\" >> config.py
+echo "USERNAME = " \"$NJT_USERNAME\" >> config.py
+echo "APIKEY = " \"$TEST_NJT_APIKEY\" >> config.py
+echo "HOSTNAME =" \"$TEST_NJT_URL\" >> config.py
