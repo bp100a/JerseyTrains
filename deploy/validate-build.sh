58d97591124341b0e480d4e8c3ac5e973b588d3e
bp100a <bp100a@hotmail.com>
2018-12-30 20:46:15 -0500

    display file if bad

--- deploy/validate-build.sh
+++ deploy/validate-build.sh
@@ -10,6 +10,7 @@
 
 aws lambda invoke --invocation-type RequestResponse --function-name JerseyTrains --qualifier STAGE --region us-east-1 --payload file://tests/data/SetHomeStation.JSON SetHomeStation.out
 if ! grep -q 'Your home station has been set to' SetHomeStation.out; then
+   echo SetHomeStation.out
    exit 1
 fi
 
