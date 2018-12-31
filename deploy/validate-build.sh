ac3fa4579e81a2bebf943bd25e097977b009cefc
bp100a <bp100a@hotmail.com>
2018-12-30 20:56:29 -0500

    add second STAGE test for Get Home Station

--- deploy/validate-build.sh
+++ deploy/validate-build.sh
@@ -14,5 +14,11 @@ if ! grep -q 'Your home station has been set to' SetHomeStation.out; then
    exit 1
 fi
 
+aws lambda invoke --invocation-type RequestResponse --function-name JerseyTrains --qualifier STAGE --region us-east-1 --payload file://tests/data/GetHomeStation.JSON GetHomeStation.out
+if ! grep -q 'Your current home station is' GetHomeStation.out; then
+   cat GetHomeStation.out
+   exit 2
+fi
+
 # nothing wrong, clean exit
 exit 0
